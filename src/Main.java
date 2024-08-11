beddb912e4a91a26cf1d697bea88e0c8d27cb879
Lev <finvancan@gmail.com>
2024-08-11 18:52:16 +0300

    BonusMiles

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 5691;
+        int bonusMiles = (int) (ticketPrice / 20);
+        System.out.println("Начисленные мили: " + bonusMiles);
+    }
+}

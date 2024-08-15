8f693124b59ab64cf38d8b85e62e63a4b2d1a5c4
Lev <finvancan@gmail.com>
2024-08-15 23:30:31 +0300

    Another commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double ticketPrice = 5692;
+        double ticketPrice = 5691;
         int bonusMiles = (int) (ticketPrice / 20);
         System.out.println("Начисленные мили: " + bonusMiles);
     }
